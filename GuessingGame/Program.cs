a3fa25b5d4b9e0985634c80b6edd5e52ad0f72a4
Thumbaleena <thumbaleena@outlook.com>
2020-10-28 20:27:40 -0400

    code push

---
+++ GuessingGame/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool realInteger = false;
+            int randomNumber;
+            int userNumberGuess = 0;
+
+            Console.WriteLine("Hello! Welcome to the Guessing Game!");
+            Console.Write("Please guess a number between 1 and 20: ");
+            string userInput = Console.ReadLine();
+
+            realInteger = int.TryParse(userInput, out userNumberGuess);
+    		//userNumberGuess = int.Parse(userInput);
+        }
+    }
+}
