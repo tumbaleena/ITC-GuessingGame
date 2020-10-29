1227754243c6d4024e4834582e9e5d2ebf0407df
Thumbaleena <thumbaleena@outlook.com>
2020-10-28 20:32:52 -0400

    added if statement for invalid input

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -7,7 +7,8 @@ namespace GuessingGame
         static void Main(string[] args)
         {
             bool realInteger = false;
-            int randomNumber;
+            Random random = new Random();
+            int randomNumber = random.Next(21);
             int userNumberGuess = 0;
 
             Console.WriteLine("Hello! Welcome to the Guessing Game!");
@@ -15,7 +16,40 @@ namespace GuessingGame
             string userInput = Console.ReadLine();
 
             realInteger = int.TryParse(userInput, out userNumberGuess);
-    		//userNumberGuess = int.Parse(userInput);
+
+            if (userNumberGuess > 20 || userNumberGuess < 1 || realInteger == false)
+            {
+                Console.WriteLine("Sorry, the number needs to be a whole number between 1 and 20.");
+            }
+
+            if (userNumberGuess < randomNumber)
+            {
+                Console.WriteLine($"Guess Higher! (more than {userNumberGuess}");
+            }
+            else if (userNumberGuess > randomNumber)
+            {
+                Console.WriteLine($"Guess Lower (less than {userNumberGuess})");
+            }
+            else
+            {
+                Console.WriteLine($"You got it, Dude!  The correct number is {userNumberGuess}");
+            }
+
+
+            /*           userNumberGuess = int.Parse(userInput);
+             *           if (userNumberGuess < randomNumber)
+                       {
+                           Console.WriteLine("Guess Higher");
+                       }
+                       else if (userNumberGuess == randomNumber)
+                       {
+                           Console.WriteLine("You got it, Dude!");
+                       }
+                       else
+                       {
+                           Console.WriteLine("Guess Lower");
+                       }
+            */
         }
     }
 }
